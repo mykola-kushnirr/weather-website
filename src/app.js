b5ab4c89a1c034f77644c8c30afb1ac569784e51
Mykola Kushnir <kushnir.mykolaa@gmail.com>
2024-07-31 10:25:32 +0300

    Some minor changes

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About',
-    name: 'Mykola, me',
+    name: 'Mykola',
   });
 });
 app.get('/help', (req, res) => {
